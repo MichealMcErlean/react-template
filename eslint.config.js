7a18e0889a5c8357e3796fab9ced3a6bf264bd19
Micheal McErlean <MichealMcErlean@users.noreply.github.com>
2025-12-01 07:39:44 +1100

    Template complete

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,11 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import vitestGlobals from 'eslint-config-vitest-globals/flat'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
+  vitestGlobals,
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
